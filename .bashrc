7f5a4e7265b06c108ec3da385f31c83ce8a9e564
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-12-16 23:29:02 -0500

    add vim config

--- .bashrc
+++ .bashrc
@@ -10,7 +10,7 @@ alias grep='grep --color=auto'
 . "$HOME/.git-prompt.sh"
 . "$HOME/.cargo/env"
 GIT_PS1_SHOWDIRTYSTATE=1
-PS1='\[\033[35m\]\u@\w\[\033[00m\]$(__git_ps1 " (%s)") \$ '
+PS1='\[\033[35m\]\u@\h:\w\[\033[00m\]$(__git_ps1 " at \[\033[34m\]%s\[\033[00m\]") \$ '
 
 echo "Welcome $(whoami)!"
 date +"%A %b %d, %Y %r"
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ DOTFILES_ROOT=$(dirname "$0")
 curl -s https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 
 echo "Configuring Bash..."
-cp $DOTFILES_ROOT/.bashrc ~/.bashrc
+cp $DOTFILES_ROOT/.bashrc $HOME/.bashrc
 
+echo "Configuring Vim..."
+cp -r $DOTFILES_ROOT/vim/* $HOME/.vim/
 echo "Done!"
